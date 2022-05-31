24299c223c7e17ac9b4c06281de6c3232343b8b4
Kavin Adarsh <86001742+Kavin-Adarsh@users.noreply.github.com>
2022-06-01 00:35:49 +0530

    Add files via upload

---
+++ 1. PointerSwap.c
@@ -0,0 +1,18 @@
+//swaping using pointer
+
+#include<stdio.h>
+
+void swap(int *a, int *b){
+    *a -= *b;
+    *b += *a;
+    *a = *b - *a;
+}
+
+int main(){
+    int a = 10;
+    int b = 20;
+
+    swap(&a, &b);
+
+    printf("%d %d", a, b);
+}
